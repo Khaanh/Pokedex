3b70de576cd4e57d2abde612ac08428f4403beff
Khanh Nguyen <Khanh13Nguyen13@gmail.com>
2022-09-20 01:37:13 +0200

    Optimize create element function

--- js/index.js
+++ js/index.js
@@ -1,15 +1,22 @@
 let url = "https://pokeapi.co/api/v2/pokemon/";
-let img = document.querySelector(".pokedex-img");
+// let img = document.querySelector(".pokedex-img");
 let holder = document.querySelector(".pokedex-holder");
 let searchControl = document.querySelector(".form-search");
 let searchBtn = document.querySelector(".form-btn");
 
 searchBtn.addEventListener("click", function () {
   let id = searchControl.value;
-  callPokemon(id, appendPokemon);
+  let flag =
+    document.querySelector(".pokedex-img") &&
+    document.querySelector(".pokedex-name") &&
+    document.querySelector(".pokedex-id")
+      ? true
+      : false;
+
+  callPokemon(id, appendPokemon, flag);
 });
 
-async function callPokemon(id, appendPokemon) {
+async function callPokemon(id, appendPokemon, status) {
   let info = {};
   try {
     let response = await fetch(`${url}${id}`);
@@ -18,31 +25,46 @@ async function callPokemon(id, appendPokemon) {
     info.src = data.sprites.other.dream_world.front_default;
     info.id = data.id;
     info.name = data.species.name;
+    console.info("data", data);
   } catch (error) {
-    error.log("error", error);
+    console.error("error", error);
   }
 
-  appendPokemon(info);
+  appendPokemon(info, status);
 }
 
-function appendPokemon(obj) {
-  let fragment = new DocumentFragment();
-  let pokedexImg = document.createElement("img");
-  let imgSrc = obj.src;
-  let pokedexName = document.createElement("div");
-  let pokedexId = document.createElement("span");
+function appendPokemon(obj, status) {
+  if (status == true) {
+    let id = document.querySelector(".pokedex-id");
+    let name = document.querySelector(".pokedex-name");
+    let img = document.querySelector(".pokedex-img img");
 
-  pokedexId.classList.add("pokedex-id");
-  pokedexName.classList.add("pokedex-name");
-  holder.appendChild(pokedexName);
-  holder.appendChild(pokedexId);
+    img.setAttribute("src", obj.src);
+    id.textContent = `ID: ${obj.id}`;
+    name.textContent = obj.name;
+  } else {
+    let fragment = new DocumentFragment();
+    let img = document.createElement("img");
+    let imgSrc = obj.src;
+    let pokedexName = document.createElement("div");
+    let pokedexId = document.createElement("span");
+    let pokedexImg = document.createElement("i");
 
-  pokedexName.textContent = obj.name;
-  pokedexId.textContent = `ID: ${obj.id}`;
-  pokedexImg.setAttribute("src", imgSrc);
+    pokedexId.classList.add("pokedex-id");
+    pokedexName.classList.add("pokedex-name");
+    pokedexImg.classList.add("pokedex-img");
 
-  img.appendChild(pokedexImg);
-  fragment.appendChild(pokedexName);
-  fragment.appendChild(pokedexId);
-  holder.appendChild(fragment);
+    holder.appendChild(pokedexImg);
+    holder.appendChild(pokedexName);
+    holder.appendChild(pokedexId);
+
+    pokedexName.textContent = obj.name;
+    pokedexId.textContent = `ID: ${obj.id}`;
+    img.setAttribute("src", imgSrc);
+
+    pokedexImg.appendChild(img);
+    fragment.appendChild(pokedexName);
+    fragment.appendChild(pokedexId);
+    holder.appendChild(fragment);
+  }
 }
